a58b9556f116048175caf966053b759d95e150a6
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2018-11-24 22:32:29 -0300

    [UPDATE] Renaming project

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace UnitOfWork
+namespace UnitOfWorkProject
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -6,12 +6,18 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using UnitOfWorkProject.Context;
+using UnitOfWorkProject.Implementation;
+using UnitOfWorkProject.Interfaces;
+using UnitOfWorkProject.Repositories;
+using UnitOfWorkProject.Services;
 
-namespace UnitOfWork
+namespace UnitOfWorkProject
 {
     public class Startup
     {
@@ -26,11 +32,25 @@ namespace UnitOfWork
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var connection = @"Server=localhost,1433;Initial Catalog=UnitOfWork; User ID=userunitofwork; Password=user@test123";
+            services.AddDbContext<StoreContext>(options => options.UseSqlServer(connection));
+            
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddScoped(typeof(ProductService));
+            services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // app.Use(async (context, next) => {
+            //     await next.Invoke();
+
+            //     var unitOfWork = (IUnitOfWork)context.RequestServices.GetService(typeof(IUnitOfWork));
+            //     await unitOfWork.Save();
+            // });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
